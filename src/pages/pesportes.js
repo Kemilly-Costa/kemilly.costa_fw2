c38db76844599882285395a1bc8d6ec0508c94b0
Kemilly-Costa <kemilly.costa2006@gmail.com>
2024-11-29 11:49:40 +0000

    tabela no pesportes

--- src/pages/pesportes.js
+++ src/pages/pesportes.js
@@ -1,19 +1,43 @@
 import { Container } from "react-bootstrap";
 import Menu from "./modulos/navbar";
 import Footer from "./modulos/footer";
+import Table from 'react-bootstrap/Table';
 
-export default function Pesportes(){
+export default function Pesportes() {
     return <>
-    <Menu/>
-    <Container>
-       <Container className="border border-sm border-primary rounded-pill text-center col-sm-6">
-         Lista de produtos de Esportes 
+        <Menu />
+        <Container>
+            <Container className="border border-sm border-primary rounded-pill text-center col-sm-6">
+                Lista de produtos de Esportes
+            </Container>
+
+            <Container className="border border-sm border-primary rounded-pill text-center col-sm-6">
+                <Table striped bordered hover>
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>PRODUTO</th>
+                            <th>CATEGORIA</th>
+                            <th>DESCRIÇÃO</th>
+                            <th>PREÇO</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+
+                        {/* LAÇO DE REPETIÇÃO
+                         */}
+
+                        <tr>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                    </tbody>
+                </Table>
+            </Container>
         </Container>
-    
-        <Container className="border border-sm border-primary rounded-pill text-center col-sm-6">
-         Os produtos
-        </Container>
-    </Container>
-    <Footer />
+        <Footer />
     </>
 }
